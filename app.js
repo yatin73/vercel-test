7a597165a8dbcde1117c7566aa49a23db6892ff0
yatin73 <60352795+yatin73@users.noreply.github.com>
2022-12-18 14:30:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', function(req,res){
 })
 
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=> console.log(`Server started on ${PORT}`));
 
